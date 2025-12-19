7bf349cb5983462ad07a1057c45a82e6f3ffcc05
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-19 21:26:13 +0500

    bajarildi

---
+++ task07.py
@@ -0,0 +1,12 @@
+parol = 'salom123'
+
+for urinish in range(3) :
+    urinish = input('parolni kiriting :')
+    if parol == urinish :
+    
+        print('topdingiz ')
+        break
+    else :
+        print('yana urinib koring :')
+else :
+    print('bloklandiz ')
