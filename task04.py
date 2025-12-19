384ac668eedb34b95f1ca71f13c93fd616ff0871
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-19 20:23:31 +0500

    bajarildi

---
+++ task04.py
@@ -0,0 +1,14 @@
+from random import randint
+
+secret = randint(1,20)
+
+topildi = False
+
+while not topildi :
+        
+        num = int(input('son :')) 
+    
+    
+if secret == num :
+    print('topdingiz ')
+    topildi = True
